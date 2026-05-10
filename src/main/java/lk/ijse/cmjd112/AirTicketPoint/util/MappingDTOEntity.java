c8f186ef0db7966fa186030a3f420a41c830136f
Sakun Dinujaya <sakundinujaya3@gmail.com>
2026-05-10 14:39:19 +0530

    Add : Airport List Code

--- src/main/java/lk/ijse/cmjd112/AirTicketPoint/util/MappingDTOEntity.java
+++ src/main/java/lk/ijse/cmjd112/AirTicketPoint/util/MappingDTOEntity.java
@@ -4,7 +4,10 @@ import lk.ijse.cmjd112.AirTicketPoint.dto.AirportDTO;
 import lk.ijse.cmjd112.AirTicketPoint.entities.Airport;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -18,5 +21,8 @@ public class MappingDTOEntity {
     }
     public Airport toAirportEntity(AirportDTO airportDTO){
         return modelMapper.map(airportDTO, Airport.class);
+    }
+    public List<AirportDTO> getAirportDTO(List<Airport> airport){
+        return modelMapper.map(airport, new TypeToken<List<AirportDTO>>(){}.getType());
     }
 }
