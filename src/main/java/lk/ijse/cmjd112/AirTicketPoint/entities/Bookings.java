94e8ab39d699b2905dca9c13b468a7865fce20aa
Sakun Dinujaya <sakundinujaya3@gmail.com>
2026-05-11 23:45:19 +0530

    Add : Data Entities

---
+++ src/main/java/lk/ijse/cmjd112/AirTicketPoint/entities/Bookings.java
@@ -0,0 +1,33 @@
+package lk.ijse.cmjd112.AirTicketPoint.entities;
+
+import jakarta.persistence.*;
+import lk.ijse.cmjd112.AirTicketPoint.dto.BookingStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity(name="booking")
+public class Bookings implements Serializable {
+    @Id
+    private String bookingId;
+    private String reference;
+    private LocalDateTime bookingDateTime;
+    private int seatCount;
+    private double totalAmount;
+    @Enumerated(EnumType.STRING)
+    private BookingStatus bookingStatus;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_Id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="flight_Id")
+    private  flightId;
+}
